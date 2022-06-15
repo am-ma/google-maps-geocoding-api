c9c50b4c9d8335a055743a495388f2de068cfc71
am-ma <ayumi.mitsunaga@sencorp.co.jp>
2022-06-15 17:15:09 +0900

    🚧  緯度・経度の取得処理

--- src/geocoding.test.ts
+++ src/geocoding.test.ts
@@ -2,9 +2,10 @@ import { getGeocode } from "./geocoding";
 
 describe('Geocoding', () => {
   describe('getGeocode', () => {
-    it('should return', () => {
-      getGeocode();
-      expect(true).toBeTruthy();
+    it('should return', async () => {
+      const result = await getGeocode("福岡県福岡市博多区博多駅前1-1");
+      expect(result.lat).toBe(33.5904778);
+      expect(result.lng).toBe(130.4172874);
     });
   });
 });
--- src/geocoding.ts
+++ src/geocoding.ts
@@ -1,27 +1,25 @@
 
 import { config } from "dotenv";
-import { Client } from "@googlemaps/google-maps-services-js"
+import { Client, LatLngLiteral } from "@googlemaps/google-maps-services-js"
 
 const getApiKey = () => config().parsed.GOOGLE_API_KEY;
 
-const getGeocode = () => {
+const getGeocode = async (address: string): Promise<LatLngLiteral> => {
   const client = new Client({});
   
-  client
-    .geocode({
+  try {
+    const result = await client.geocode({
       params: {
-        address: "福岡県福岡市博多区博多駅前1-1",
+        address,
         key: getApiKey(),
       },
       timeout: 500, // milliseconds
-    })
-    .then(r => {
-      const location = r.data.results[0].geometry.location;
-      console.log(location.lat, location.lng);
-    })
-    .catch(e => {
-      console.log(e);
     });
+
+    return result.data.results[0].geometry.location;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export {
