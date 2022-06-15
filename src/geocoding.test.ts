1d619fbc6dda4788c1d1a907e490641df9faeea7
am-ma <ayumi.mitsunaga@sencorp.co.jp>
2022-06-15 17:07:01 +0900

    🚧  geocoding を動かす

--- src/geocoding.test.ts
+++ src/geocoding.test.ts
@@ -1,6 +1,9 @@
+import { getGeocode } from "./geocoding";
+
 describe('Geocoding', () => {
   describe('getGeocode', () => {
     it('should return', () => {
+      getGeocode();
       expect(true).toBeTruthy();
     });
   });
--- src/geocoding.ts
+++ src/geocoding.ts
@@ -1,5 +1,27 @@
+
+import { config } from "dotenv";
+import { Client } from "@googlemaps/google-maps-services-js"
+
+const getApiKey = () => config().parsed.GOOGLE_API_KEY;
+
 const getGeocode = () => {
-  console.log("test");
+  const client = new Client({});
+  
+  client
+    .geocode({
+      params: {
+        address: "福岡県福岡市博多区博多駅前1-1",
+        key: getApiKey(),
+      },
+      timeout: 500, // milliseconds
+    })
+    .then(r => {
+      const location = r.data.results[0].geometry.location;
+      console.log(location.lat, location.lng);
+    })
+    .catch(e => {
+      console.log(e);
+    });
 };
 
 export {
