7730299dbcba035c83938f97d4122515d3070f2e
am-ma <ayumi.mitsunaga@sencorp.co.jp>
2022-06-16 10:58:42 +0900

    ♻ エラー処理を調整

--- src/geocoding.ts
+++ src/geocoding.ts
@@ -1,15 +1,14 @@
-
 import { config } from "dotenv";
 import * as csv from "csv";
-import { Client, LatLngLiteral } from "@googlemaps/google-maps-services-js"
+import * as fs from "fs";
+import { Client, LatLngLiteral } from "@googlemaps/google-maps-services-js";
 import { TAddressCsvRow } from "./type";
-const fs = require("fs");
 
 const getApiKey = () => config().parsed.GOOGLE_API_KEY;
 
 const getGeocode = async (address: string): Promise<LatLngLiteral> => {
   const client = new Client({});
-  
+
   try {
     const result = await client.geocode({
       params: {
@@ -22,6 +21,7 @@ const getGeocode = async (address: string): Promise<LatLngLiteral> => {
     return result.data.results[0].geometry.location;
   } catch (error) {
     console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -30,7 +30,7 @@ const getGeocodeByAddressCsv = async (csvPath: string) => {
     const addressCsv = await loadAddressesFromCsv(csvPath);
 
     const data = addressCsv.map(async (addressRow): Promise<TAddressCsvRow> => {
-      const {lat, lng} = await getGeocode(addressRow.address);
+      const { lat, lng } = await getGeocode(addressRow.address);
 
       return {
         ...addressRow,
@@ -41,77 +41,77 @@ const getGeocodeByAddressCsv = async (csvPath: string) => {
 
     await exportCsv(await Promise.all(data));
   } catch (error) {
+    console.log(error);
     throw new Error(error);
   }
 };
 
-const loadAddressesFromCsv = (csvPath: string): Promise<TAddressCsvRow[]> => 
+const loadAddressesFromCsv = (csvPath: string): Promise<TAddressCsvRow[]> =>
   new Promise((resolve, reject) => {
     const data = [];
     fs.createReadStream(csvPath)
-      .pipe(csv.parse((_, records) => {
-        const csvHeader = records[0] as string[];
-        const csvBody = records.slice(1) as string[][];
-        const invalid = csvBody.some(row => 
-          row.length !== csvHeader.length
-          || csvHeader[0] !== 'address'
-          || row[0] === ''
-        );
-        if (invalid) {
-          throw new Error("CSVフォーマットが不正です");
-        }
-      
-        const addresses = csvBody.map((row): TAddressCsvRow => {
-          const result: TAddressCsvRow = {address: ''};
-          row.forEach((col, index) => {
-            result[csvHeader[index]] = col;
+      .pipe(
+        csv.parse((_, records) => {
+          const csvHeader = records[0] as string[];
+          const csvBody = records.slice(1) as string[][];
+          const invalid = csvBody.some(
+            (row) =>
+              row.length !== csvHeader.length ||
+              csvHeader[0] !== "address" ||
+              row[0] === ""
+          );
+          if (invalid) {
+            throw new Error("CSVフォーマットが不正です");
+          }
+
+          const addresses = csvBody.map((row): TAddressCsvRow => {
+            const result: TAddressCsvRow = { address: "" };
+            row.forEach((col, index) => {
+              result[csvHeader[index]] = col;
+            });
+
+            return result;
           });
-      
-          return result;
-        });
-      
-        data.push(...addresses);
-      }))
-      .on('error', error => {
+
+          data.push(...addresses);
+        })
+      )
+      .on("error", (error) => {
         reject(error);
       })
-      .on('end', () => {
+      .on("end", () => {
         resolve(data);
       });
   });
 
-const exportCsv = (addressCsv: TAddressCsvRow[]): Promise<void> => 
+const exportCsv = (addressCsv: TAddressCsvRow[]): Promise<void> =>
   new Promise((resolve, reject) => {
     if (addressCsv.length <= 0) {
       throw new Error("CSVが空です。");
-    } 
+    }
     const csvData = [
       Object.keys(addressCsv[0]),
-      ...addressCsv.map(addressRow => Object.values(addressRow)),
+      ...addressCsv.map((addressRow) => Object.values(addressRow)),
     ];
-    console.log(csvData);
     csv.stringify(csvData, (error, output) => {
       if (error) {
         reject(error);
         return;
       }
       const dateNow = new Date();
-      const now = `${dateNow.getFullYear()}${dateNow.getMonth() + 1}${dateNow.getDate()}${dateNow.getHours()}${dateNow.getMinutes()}${dateNow.getSeconds()}`;
+      const now = `${dateNow.getFullYear()}${
+        dateNow.getMonth() + 1
+      }${dateNow.getDate()}${dateNow.getHours()}${dateNow.getMinutes()}${dateNow.getSeconds()}`;
       const fileName = `geocode_${now}.csv`;
-      fs.writeFile(fileName, output, (error) => {
-        if (error) {
-          reject(error);
+      fs.writeFile(fileName, output, (er) => {
+        if (er) {
+          reject(er);
           return;
         }
-        console.log(fileName+'を出力しました。');
+        console.log(fileName + "を出力しました。");
         resolve();
       });
     });
   });
 
-export {
-  getGeocode,
-  getGeocodeByAddressCsv,
-  loadAddressesFromCsv,
-  exportCsv,
-};
+export { getGeocode, getGeocodeByAddressCsv, loadAddressesFromCsv, exportCsv };
